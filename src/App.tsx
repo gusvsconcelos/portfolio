6198ad0cf711101d8f6adc1ebf4e533507954c3e
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-02-17 12:33:28 -0300

    feat: add antialiased property

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="w-full min-h-screen p-4 bg-bg-1 font-display">
+    <div className="w-full min-h-screen p-4 bg-bg-1 font-display **:antialiased">
       <div className="flex flex-col gap-8 max-w-150 mx-auto">
         <Home />
       </div>
