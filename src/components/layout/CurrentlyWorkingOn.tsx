14615eb5d58e29bebad3e3cde63df35a2b24c53b
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-04-11 11:46:15 -0300

    feat: add currently working on section

---
+++ src/components/layout/CurrentlyWorkingOn.tsx
@@ -0,0 +1,23 @@
+export default function CurrentlyWorkingOn() {
+  return (
+    <section className="p-6 rounded-sm border-solid border border-fg-2">
+      {/*Title*/}
+      <h2 className="text-2xl font-semibold text-accent-1">
+        Em desenvolvimento
+      </h2>
+
+      {/*Working on*/}
+      <div className="flex flex-wrap gap-4 mt-4 *:flex *:items-center *:gap-2 *:text-fg-2 *:font-semibold *:animate-pulse">
+        <div>
+          <div className="w-2 h-2 rounded-full bg-accent-2"></div>
+          <p>Construindo Portfólio</p>
+        </div>
+
+        <div>
+          <div className="w-2 h-2 rounded-full bg-accent-2"></div>
+          <p>Estudando PHP</p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,13 @@ import About from "../components/layout/About";
 import Contact from "../components/layout/Contact";
 import Footer from "../components/layout/Footer";
 import ScrollToTop from "../components/ui/ScrollToTop";
+import CurrentlyWorkingOn from "../components/layout/CurrentlyWorkingOn";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <CurrentlyWorkingOn />
       <Projects />
       <About />
       <Contact />
