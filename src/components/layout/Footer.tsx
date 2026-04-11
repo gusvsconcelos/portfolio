6670ed62ec9d59bfd2698abf5a355336db6445a9
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-04-11 10:28:15 -0300

    refactor: simplify footer structure

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,16 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center gap-2 p-6 rounded-sm border-solid border border-fg-2">
-      {/*Title*/}
-      <h2 className="text-lg text-accent-1">Gustavo | Dev</h2>
-
-      {/*Copyright*/}
-      <p className="text-sm text-fg-2 *:hover:text-accent-1 *:transition *:duration-300">
-        Powered by{" "}
-        <a href="https://www.linkedin.com/in/gusvsconcelos/" target="_blank">
-          Gustavo Vasconcelos
-        </a>
-      </p>
+    <footer className="m-auto p-6">
+      <a
+        href="https://www.linkedin.com/in/gusvsconcelos/"
+        target="_blank"
+        className="text-fg-2 hover:text-accent-1 transition duration-300"
+      >
+        &copy; {new Date().getFullYear()} Gustavo Vasconcelos
+      </a>
     </footer>
   );
 }
