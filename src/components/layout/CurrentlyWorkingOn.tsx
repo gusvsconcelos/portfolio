5d16e04d1cd2927a7c08ac3ca571bf0be74c5591
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-04-13 12:55:28 -0300

    refactor: update currently-working-on section title to reduce ambiguity

--- src/components/layout/CurrentlyWorkingOn.tsx
+++ src/components/layout/CurrentlyWorkingOn.tsx
@@ -2,9 +2,7 @@ export default function CurrentlyWorkingOn() {
   return (
     <section className="p-6 rounded-sm border-solid border border-fg-2">
       {/*Title*/}
-      <h2 className="text-2xl font-semibold text-accent-1">
-        Em desenvolvimento
-      </h2>
+      <h2 className="text-2xl font-semibold text-accent-1">Em andamento</h2>
 
       {/*Working on*/}
       <div className="flex flex-wrap gap-4 mt-4 *:flex *:items-center *:gap-2 *:text-fg-2 *:font-semibold *:animate-pulse">
@@ -15,7 +13,7 @@ export default function CurrentlyWorkingOn() {
 
         <div>
           <div className="w-2 h-2 rounded-full bg-accent-2"></div>
-          <p>Estudando PHP</p>
+          <p>Estudando PHP (Laravel)</p>
         </div>
       </div>
     </section>
