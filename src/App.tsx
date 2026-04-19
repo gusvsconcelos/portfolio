bb6d9cb71fcd311ea8276eed589866e8dad378f0
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-04-18 23:57:43 -0300

    feat: add sticky-to-bottom behavior to footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="w-full min-h-screen p-3 sm:p-6 bg-bg-1 dark:bg-bg-dark-1 font-display **:antialiased">
-      <div className="flex flex-col gap-3 sm:gap-6 max-w-150 mx-auto">
-        <Home />
-      </div>
+    <div className="flex flex-col min-h-screen p-3 sm:p-6 bg-bg-1 dark:bg-bg-dark-1 font-display antialiased">
+      <main className="flex-1 w-full">
+        <div className="flex flex-col gap-3 sm:gap-6 max-w-150 mx-auto">
+          <Home />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import CurrentlyWorkingOn from "../components/layout/CurrentlyWorkingOn";
 import Projects from "../components/layout/Projects";
 import About from "../components/layout/About";
 import Contact from "../components/layout/Contact";
-import Footer from "../components/layout/Footer";
 import ScrollToTop from "../components/ui/ScrollToTop";
 import ThemeToggle from "../components/ui/ThemeToggle";
 
@@ -15,7 +14,6 @@ export default function Home() {
       <Projects />
       <About />
       <Contact />
-      <Footer />
       <ScrollToTop />
       <ThemeToggle />
     </>
