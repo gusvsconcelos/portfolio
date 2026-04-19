04de86b2aac3c1166954544351f969bc6beb85c0
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-04-19 00:33:41 -0300

    fix: project thumbnails not loading on prod

---
+++ src/utils/thumbnails.ts
@@ -0,0 +1,7 @@
+import MovieRoulette from "/src/assets/thumbnails/movie-roulette.png";
+import n8nAssistant from "/src/assets/thumbnails/n8n-assistant.png";
+import Dailyway from "/src/assets/thumbnails/dailyway.png";
+import StakeLab from "/src/assets/thumbnails/stakelab.png";
+import weatherPy from "/src/assets/thumbnails/weather-py.png";
+
+export { MovieRoulette, n8nAssistant, Dailyway, StakeLab, weatherPy };
